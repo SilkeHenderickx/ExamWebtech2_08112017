9c966d0ca1fe89fb33a324be8a3cb8ef75ba53f0
SilkeHenderickx <silke.henderickx@student.ap.be>
2017-11-08 16:34:19 +0100

    look up actor in IMDB

--- movieApp/app/_attachments/scripts/app.js
+++ movieApp/app/_attachments/scripts/app.js
@@ -13,30 +13,57 @@ angular.module('movieApp', ['ngRoute'])
             });
             
     })
-    .controller('homeCtrl', function ($scope, actorCouchdbSrv) {
+    .controller('homeCtrl', function ($scope, actorCouchdbSrv, actorIMDBSrv) {
     	$('#searchButton').on('click', function(e){
     		var actor = $('#actor').val();
-    		var ALL_DOCS = '../../_all_docs?include_docs=true';
+    		var BY_ACTOR = '_view/byActor';
+    		var IMDB = 'http://theimdbapi.org/api/find/person?name=';
     		
-    		
-    		actorCouchdbSrv.getCouchdbData(ALL_DOCS, actor).then(function(data){
+    		actorCouchdbSrv.getCouchdbData(BY_ACTOR, actor).then(function(data){
     			
     			console.log(data);
-    			if(data == undefined){
+    			if(data.data.total_rows == 0){
     				console.log("actor does not yet exist");
-    			}
-    		})
-    	});
+    				actorIMDBSrv.getIMDBData(IMDB, actor).then(function(data){
+    	    			console.log(data);
+    	    		})
+    				}
+    			});
+    		
+    	})
+    })
+    .service('actorIMDBSrv', function($http, $q){
+    	
+    	var q = $q.defer();
+    	
+    	this.getIMDBData = function(url, actor){
+    		var url = url + actor;
+    		
+    		
+    		$http.get(url)
+    		.then(function(data){
+    			q.resolve(data.data[0].filmography.actor);
+    		}, function error(err){
+    			q.reject(err);
+    			console.log(err);
+    		});
+    		return q.promise;
+    	}
     })
     .service('actorCouchdbSrv', function($http, $q){
     	
     	var q = $q.defer();
     	
+    	
+    	
     	this.getCouchdbData = function(url, actor){
     		
+    		
+    		
+    		console.log(url);
     		$http.get(url)
     			.then(function(data){
-    				q.resolve(data.data[0]);
+    				q.resolve(data);
     			}, function error(err){
     				q.reject(err);
     				console.log(err);
